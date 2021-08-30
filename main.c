1da0dedc421f1f84eea970b4a24d2f309a7ceb81
FacundoPiaggio <82190900+FacundoPiaggio@users.noreply.github.com>
2021-08-29 22:44:22 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,25 @@
-# RepostorioPrueba
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <scanner.h>
+
+
+int main()
+{
+
+    char buffer[128];
+    int i=0;
+    char c;
+    do
+    {
+        c = getchar();
+        buffer[i] = c;
+        i++;
+    }while(c!='\n');
+
+    buffer[i] = '\0';
+
+    get_token(buffer);
+
+    return 0;
+}
